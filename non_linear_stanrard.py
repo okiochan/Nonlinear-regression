ca457025a3e6f43346a8510e01799c24af715b1d
okiochan <maryalx@mail.ru>
2017-12-24 00:59:57 +0300

    uu

--- non_linear_stanrard.py
+++ non_linear_stanrard.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import math
+import matplotlib.patches as mpatches
 
 def GetData(dev = 0.1):
     np.random.seed(271828182)
@@ -57,17 +58,33 @@ def GradDescent(X,Y, Max,etta):
         print(Q)
     return x
 
+def Linear(X,Y):
+    X = np.atleast_2d(X).T
+    l = X.shape[0]
+    n = X.shape[1]
 
+    # #concatenate ones in front of matrix
+    ones = np.atleast_2d(np.ones(l)).T
+    X = np.concatenate((ones,X),axis=1)
+    # learn
+    res = np.linalg.pinv(X.T.dot(X)).dot(X.T.dot(y))
+    return res[1:(n+1)], res[0]
 
 x, y = GetData(0.3)
 
 Y1 = np.zeros(y.size)
-Xh = GradDescent(x,y,2000,0.002)
+Y2 = np.zeros(y.size)
+
+W = GradDescent(x,y,2000,0.002)
+w = Linear(x,y)
 
 for i in range(y.size):
-    Y1[i] = F(Xh,x[i])
+    Y1[i] = F(W,x[i])
+    Y2[i] = w[0] * x[i] + w[1]
 
 plt.scatter(x,y)
 plt.plot(x, Y1, label='y pred', color = "orange")
+plt.plot(x, Y2, label='y pred', color = "violet")
+plt.legend(handles=[mpatches.Patch(color='orange', label='nonlinear (sinus)'),mpatches.Patch(color='violet', label='linear')])
 plt.show()
 
